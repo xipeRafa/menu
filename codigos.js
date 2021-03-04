b5b97d5fc105c0c5624798977b25e44b71e3b238
rafa <rafaelsistem87@gmail.com>
2021-03-03 19:18:29 -0700

    my menu css

---
+++ codigos.js
@@ -0,0 +1,29 @@
+function toggleColorcero() {
+    document.getElementById("btncero").classList.toggle("mostrar");
+    document.getElementById("btncero").classList.toggle("ocultar");
+}
+
+function toggleColor() {
+    document.getElementById("btn1").classList.toggle("mostrar");
+    document.getElementById("btn1").classList.toggle("ocultar");
+}
+
+function toggleColordos() {
+    document.getElementById("btn2").classList.toggle("mostrar");
+    document.getElementById("btn2").classList.toggle("ocultar");
+}
+
+function toggleColortres() {
+    document.getElementById("btn3").classList.toggle("mostrar");
+    document.getElementById("btn3").classList.toggle("ocultar");
+}
+
+function toggleColorcuatro() {
+    document.getElementById("btn4").classList.toggle("mostrar");
+    document.getElementById("btn4").classList.toggle("ocultar");
+}
+
+function toggleColorcinco() {
+    document.getElementById("btn5").classList.toggle("mostrar");
+    document.getElementById("btn5").classList.toggle("ocultar");
+}
